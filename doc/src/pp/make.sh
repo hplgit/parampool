6499339e8c315180447c37896ea0fef23c56584b
Hans Petter Langtangen <hpl@simula.no>
2015-04-09 02:55:58 -0700

    updates

--- doc/src/pp/make.sh
+++ doc/src/pp/make.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 name=pp
-doconce format html $name --html_style=bloodish
+doconce format html $name --html_style=bootstrap_bluegray --html_code_style=inherit
+doconce split_html $name.html --pagination
 
-doconce format pdflatex $name
-doconce ptex2tex $name envir=minted
+doconce format pdflatex $name --latex_code_style=pyg
 pdflatex -shell-escape $name
 pdflatex -shell-escape $name
+
+# Publish
+dest=../../pub
+cp -r fig-pp $name.pdf $name.html ._${name}*.html $dest
